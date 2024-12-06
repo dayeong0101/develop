e3eb6a86de582c8573be38aaa078e2548826c1fe
dayeong0101 <kdy7376@kookmin.ac.kr>
2024-12-06 13:32:48 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,13 @@
 #include "include/print.h"  
 #include "include/input.h"  
 
-#define PASS (1)
-#define SKIP (0)
-#define FAIL (-1)
-
 int main() 
 {
     int num = get_Input();  
 
     if (num < 1 || num > 100) {
         printf("Input Error\n");
-        return PASS;  
+        return 0;  
     }
 
     print_Result(num);  
